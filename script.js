f443e202b27b6332fd20757fce334a761f3d05c5
Andreashoj <32867811+Andreashoj@users.noreply.github.com>
2019-01-27 12:58:17 +0100

    animation updated

---
+++ script.js
@@ -0,0 +1,30 @@
+const frontpageHeader = document.querySelectorAll('#frontpage-h1');
+const aboutHeader = document.querySelectorAll('#about-h1');
+const portfolioHeader = document.querySelectorAll('#portfolio-h1')
+const contactHeader = document.querySelectorAll('#contact-h1')
+
+window.addEventListener('load', (e) => {
+    for(let i = 0; i < frontpageHeader.length; i++) {
+        setTimeout(() => {
+            frontpageHeader[i].classList += 'frontpage-transition';  
+        }, 500)
+    }
+})
+
+window.addEventListener('load', (e) => {
+    setTimeout(() => {
+        aboutHeader[0].classList += 'frontpage-transition';  
+    }, 500) 
+})
+
+window.addEventListener('load', (e) => {
+    setTimeout(() => {
+        portfolioHeader[0].classList += 'frontpage-transition';  
+    }, 500) 
+})
+
+window.addEventListener('load', (e) => {
+    setTimeout(() => {
+        contactHeader[0].classList += 'frontpage-transition';  
+    }, 500) 
+})
